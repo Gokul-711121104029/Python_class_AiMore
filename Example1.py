08328fe1ddd15c1f843c2646a1c5fcad5804ae47
Gokul-711121104029 <gokulsc13082004@gmail.com>
2026-06-10 11:57:00 +0530

    conditional statemnts example1

---
+++ Example1.py
@@ -0,0 +1,8 @@
+# Check whether a number is positive, negative, or zero.
+num=int(input("Enter the number:"))
+if(num>0):
+    print("f{num} is positive number")
+elif(num<0):
+    print(f"{num} is negative number")
+else:
+    print("It is Zero")
---
+++ Example2.py
@@ -0,0 +1,6 @@
+# Check whether a number is even or odd.
+a=int(input("Enter your age:"))
+if(a>=18):
+    print("Your are eligible to vote")
+else:
+    print("Your are not eligible to vote")
